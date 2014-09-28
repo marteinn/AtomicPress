80ce34da5735cee52c420e5d60d1b8759cd1f2c8
Martin Sandström <martin@marteinn.se>
2014-09-28 23:02:33 +0200

    Renamed url helper. Updated pre line height

--- atomicpress/context_processors.py
+++ atomicpress/context_processors.py
@@ -2,21 +2,12 @@ from atomicpress.app import app
 from flask import request, url_for
 
 
-def is_showing(route=None, **kwargs):
+def is_url_showing(route=None, **kwargs):
     url = url_for(route, **kwargs)
     if request.path == url:
         return "active"
     return ""
 
 
-app.jinja_env.globals.update(is_showing=is_showing)
+app.jinja_env.globals.update(is_url_showing=is_url_showing)
 
-
-"""
-@app.context_processor
-def example(slug=None):
-    return {}
-
-
-app.jinja_env.globals.update(example=example)
-"""
