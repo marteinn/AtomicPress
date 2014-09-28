97705dca6437adbeb8a7be727cccfe69d431dfa5
Martin Sandström <martin@marteinn.se>
2014-09-28 22:50:21 +0200

    Created helper for applying activation classes when url is showing

--- atomicpress/context_processors.py
+++ atomicpress/context_processors.py
@@ -1,4 +1,16 @@
 from atomicpress.app import app
+from flask import request, url_for
+
+
+def is_showing(route=None, **kwargs):
+    url = url_for(route, **kwargs)
+    if request.path == url:
+        return "active"
+    return ""
+
+
+app.jinja_env.globals.update(is_showing=is_showing)
+
 
 """
 @app.context_processor
