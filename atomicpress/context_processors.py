a42cd852f135e44a4738dcdd051e65fbd5e46b51
Martin Sandström <martin@marteinn.se>
2014-09-29 00:08:25 +0200

    Updated url matching

--- atomicpress/context_processors.py
+++ atomicpress/context_processors.py
@@ -3,9 +3,10 @@ from flask import request, url_for
 
 
 def is_url_showing(route=None, **kwargs):
-    url = url_for(route, **kwargs)
-    urls = (app.config["FREEZER_BASE_URL"].strip("/")+request.path,
-            request.path,)
+    url = url_for(route, **kwargs).strip("/")
+    freeze_url = app.config["FREEZER_BASE_URL"].strip("/")+request.path
+    urls = (freeze_url.strip("/"),
+            request.path.strip("/"),)
 
     if url in urls:
         return "active"
