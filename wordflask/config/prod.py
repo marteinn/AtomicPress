1aab1473d82526b2a33f4a2344c27cb20f7089e1
Martin Sandström <martin@marteinn.se>
2014-09-28 16:49:32 +0200

    Updated config

--- wordflask/config/prod.py
+++ wordflask/config/prod.py
@@ -1,4 +1,3 @@
-import os
 from unipath import Path
 
 PROJECT_ROOT = Path(__file__).ancestor(2)
@@ -6,5 +5,9 @@ PROJECT_ROOT = Path(__file__).ancestor(2)
 DEBUG = False
 SECRET_KEY = "asdjhashjdhjasdhjsahjk"
 DATABASE_PATH = "website.db"
-SQLALCHEMY_DATABASE_URI = "sqlite:////%s" % (PROJECT_ROOT.child("blog.db"),)
+DB_PATH = PROJECT_ROOT.child("blog.db")
+SQLALCHEMY_DATABASE_URI = "sqlite:////%s" % DB_PATH
 UPLOADS_PATH = PROJECT_ROOT.child("uploads")
+
+STATIC_URL = '/static/'
+UPLOADS_URL = '/uploads/'
