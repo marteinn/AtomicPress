97ae6302d7a58e696c1ed40e4804fb92d15559ec
Martin Sandström <martin@marteinn.se>
2014-10-05 18:00:58 +0200

    Updated requirements

--- atomicpress/__init__.py
+++ atomicpress/__init__.py
@@ -9,8 +9,8 @@ atomicpress
 """
 
 __title__ = 'atomicpress'
-__version__ = '1.0'
-__build__ = 10
+__version__ = '1.0.3'
+__build__ = 103
 __author__ = 'Martin Sandström'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2014 Martin Sandström'
