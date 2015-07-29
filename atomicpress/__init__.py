0606353eb8d49de63d55129b973dd8e6523fffd2
Martin Sandström <martin@marteinn.se>
2015-07-29 17:44:01 +0200

    Updated licenses, added pandadoc as a requirement

--- atomicpress/__init__.py
+++ atomicpress/__init__.py
@@ -12,5 +12,5 @@ __version__ = '1.0.7'
 __build__ = 107
 __author__ = 'Martin Sandström'
 __license__ = 'MIT'
-__copyright__ = 'Copyright 2014 Martin Sandström'
+__copyright__ = 'Copyright 2014-2015 Martin Sandström'
 
