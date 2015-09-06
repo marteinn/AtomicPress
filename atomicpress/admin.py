7f6e1aaadf234c566c7c71dda2cc8ad564f34aab
Martin Sandström <martin@marteinn.se>
2015-09-06 17:47:06 +0200

    Added post search

--- atomicpress/admin.py
+++ atomicpress/admin.py
@@ -16,6 +16,7 @@ class HomeView(AdminIndexView):
 
 class PostView(ModelView):
     column_default_sort = ('date', True)
+    column_searchable_list = ('name', 'title', 'content')
     list_columns = ('name', 'status', 'modified', 'date', 'type')
 
 
