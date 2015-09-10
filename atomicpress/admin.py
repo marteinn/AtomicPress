bc4099db6bf8ae1ea3eec33d8b707a1a81c1789a
Martin Sandström <martin@marteinn.se>
2015-09-10 21:08:22 +0200

    Fixed deprecation warning in flask-admin

--- atomicpress/admin.py
+++ atomicpress/admin.py
@@ -17,7 +17,7 @@ class HomeView(AdminIndexView):
 class PostView(ModelView):
     column_default_sort = ('date', True)
     column_searchable_list = ('name', 'title', 'content')
-    list_columns = ('title', 'status', 'modified', 'date', 'type')
+    column_list = ('title', 'status', 'modified', 'date', 'type')
 
 
 def create_admin():
