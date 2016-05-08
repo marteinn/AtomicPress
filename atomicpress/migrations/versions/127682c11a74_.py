7fd40e93af041782c21a9150c69d9e7dbd8368a7
Martin Sandström <martin@marteinn.se>
2016-05-08 20:55:05 +0200

    Removed down-revision

--- atomicpress/migrations/versions/127682c11a74_.py
+++ atomicpress/migrations/versions/127682c11a74_.py
@@ -8,7 +8,7 @@ Create Date: 2014-09-28 18:39:53.592751
 
 # revision identifiers, used by Alembic.
 revision = '127682c11a74'
-down_revision = '3c5a7e50a629'
+down_revision = None
 
 from alembic import op
 import sqlalchemy as sa
