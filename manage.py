b8a0ca9c9c327f062833fc4a401a8ac0baccf6d1
Martin Sandström <martin@marteinn.se>
2016-05-14 21:23:23 +0200

    Changed string formatting for management

--- manage.py
+++ manage.py
@@ -15,7 +15,7 @@ PROJECT_ROOT = os.path.dirname(os.path.dirname(__file__))
 DB_PATH = os.path.join(PROJECT_ROOT, "blog.db")
 UPLOADS_PATH = os.path.join(PROJECT_ROOT, "uploads")
 
-SQLALCHEMY_DATABASE_URI = "sqlite:////%s" % DB_PATH
+SQLALCHEMY_DATABASE_URI = "sqlite:////{0}".format(DB_PATH)
 UPLOADS_PATH = "./"
 
 if __name__ == "__main__":
